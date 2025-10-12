95436f1790f5fb77c2310502f70f6b8adbdde618
Aaron Roodhart <aaronroodhart@gmail.com>
2025-10-12 16:23:13 -0400

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/aaron-roodhart-timeline/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
