de755e22983e392c02f8225eeca8a95599d754d5
Aaron Roodhart <aaronroodhart@gmail.com>
2025-11-08 13:20:43 -0500

    Update base path for new repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/aaron-roodhart-timeline/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Aaron-Roodhart-portfolio-website-/' : '/',
   plugins: [react()],
   optimizeDeps: {
     include: ['react', 'react-dom'],
