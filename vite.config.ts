e0b094d596178de99425fdcdb9b0c956beb69201
Aaron Roodhart <aaronroodhart@gmail.com>
2025-11-08 14:35:36 -0500

    Add custom domain aaronroodhart.com and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Aaron-Roodhart-portfolio-website-/' : '/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     include: ['react', 'react-dom'],
